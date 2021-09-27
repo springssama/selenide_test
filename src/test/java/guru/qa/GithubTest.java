e918dcc645af564581bcaf8563dd1497c8415493
Nadezhda <springsama@gmail.com>
2021-09-27 14:58:40 +0300

    add pageObject

--- src/test/java/guru/qa/GithubTest.java
+++ src/test/java/guru/qa/GithubTest.java
@@ -1,17 +1,17 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import guru.qa.page.GithubPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
-
 public class GithubTest {
+
+    guru.qa.page.GithubPage githubPage = new GithubPage();
+    String githubUrl = "https://github.com/";
+    String selenideName = "selenide";
+    String searchName = "SoftAssertions";
+
     @BeforeAll
     static void beforeAl() {
         Configuration.startMaximized = true;
@@ -19,14 +19,9 @@ public class GithubTest {
 
     @Test
     void openSoftAssertionPageInGithub() {
-        open("https://github.com/");
-        $(".js-site-search-form").click();
-        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
-        $$("ul.repo-list li").first().$("a").click();
-        $("[data-content='Wiki']").click();
-        $(".js-wiki-more-pages-link").click();
-        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
-        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+        githubPage.openPage(githubUrl);
+        githubPage.searchSelenide(selenideName);
+        githubPage.searchSortAssertions(searchName);
+        githubPage.verifyResults();
     }
 }
